38fbfd370ed34cceef5b525d6694329fb9cd72c3
tinhpt38 <tinhpt.38@gmail.com>
2019-06-18 18:05:47 +0700

    All full screen

---
+++ demoapp/lib/home_page.dart
@@ -0,0 +1,77 @@
+import 'package:demoapp/square_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        backgroundColor: Color(0xff52A1A9),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SquarePage()));
+              })
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              flex: 6,
+              child: Container(
+                width: size.width,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  "NO NAMES",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                decoration: BoxDecoration(color: Color(0xff52A1A9)),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                width: size.width,
+                child: Center(
+                  child: Text(
+                    "LUCI",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black, fontSize: 40),
+                  ),
+                ),
+                decoration: BoxDecoration(color: Colors.white),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                alignment: Alignment.bottomCenter,
+                width: size.width,
+                child: Text(
+                  "COM",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                decoration: BoxDecoration(color: Color(0xffDE5F4C)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- demoapp/lib/main.dart
+++ demoapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demoapp/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,73 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Demo"),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              flex: 6,
-              child: Container(
-                width: size.width,
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: Text(
-                  "NO NAMES",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                decoration: BoxDecoration(color: Color(0xff52A1A9)),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                width: size.width,
-                child: Center(
-                  child: Text(
-                    "LUCI",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.black, fontSize: 40),
-                  ),
-                ),
-                decoration: BoxDecoration(color: Colors.white),
-              ),
-            ),
-            Expanded(
-              flex: 6,
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                alignment: Alignment.bottomCenter,
-                width: size.width,
-                child: Text(
-                  "COM",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                decoration: BoxDecoration(color: Color(0xffDE5F4C)),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
