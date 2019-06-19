d91f614fffb7a979f5677570b47cee218fd55836
Demon988 <51952262+Demon988@users.noreply.github.com>
2019-06-19 22:16:19 +0700

    ádasdasdas

---
+++ demoapp/lib/my_color_item.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class MyColorItem extends StatefulWidget {
+  @override
+  _MyColorItemState createState() => _MyColorItemState();
+}
+
+class _MyColorItemState extends State<MyColorItem> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Title"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Color(0xffF16C52),
+                      child: Center(
+                        child: Text(
+                          "asũgo",
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Color(0xff252847),
+                      child: Center(
+                        child: Text(
+                          "asũgo",
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Color(0xffFFD984),
+                      child: Center(
+                        child: Text(
+                          "asũgo",
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Color(0xff7EB8C4),
+                      child: Center(
+                        child: Text(
+                          "asũgo",
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
