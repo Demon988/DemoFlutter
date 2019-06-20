27ea5686418d4800206014c7cc17d5d207281ae9
tinhpt38 <tinhpt.38@gmail.com>
2019-06-20 14:10:25 +0700

    edit title appbar1

--- demoapp/lib/my_color_item.dart
+++ demoapp/lib/my_color_item.dart
@@ -11,7 +11,7 @@ class _MyColorItemState extends State<MyColorItem> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Lam"),
+        title: Text("My Lam 123"),
         backgroundColor: Colors.blueAccent,
       ),
       body: Container(
